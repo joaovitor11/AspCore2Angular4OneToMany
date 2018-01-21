9a275238dfd9dfa16cece57cb8fc516a12836330
joao <joao.vitor96silva@gmail.com>
2018-01-21 15:07:12 -0200

    Adicionadas rotas no menu para o acesso da página do Departamento.

--- AspCore2Angular4OneToMany/ClientApp/app/app.shared.module.ts
+++ AspCore2Angular4OneToMany/ClientApp/app/app.shared.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { DepartamentoComponent } from './components/Departamento/departamento.component';
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        DepartamentoComponent
     ],
     imports: [
         CommonModule,
@@ -24,6 +26,7 @@ import { CounterComponent } from './components/counter/counter.component';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'departamento', component: DepartamentoComponent },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
