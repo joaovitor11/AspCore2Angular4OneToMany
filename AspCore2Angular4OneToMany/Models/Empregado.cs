c5dfc6eac53e9130ce2a33c05203ee88ec13bd5a
joao <joao.vitor96silva@gmail.com>
2018-01-21 13:25:24 -0200

    Adicionar arquivos de projeto.

---
+++ AspCore2Angular4OneToMany/Models/Empregado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCore2Angular4OneToMany.Models
+{
+    public partial class Empregado
+    {
+        public int EmpregadoId { get; set; }
+        public int DepartamentoId { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+
+        public Departamento Departamento { get; set; }
+    }
+}
---
+++ AspCore2Angular4OneToMany/Models/db_teste1Context.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AspCore2Angular4OneToMany.Models
+{
+    public partial class db_teste1Context : DbContext
+    {
+        public virtual DbSet<Departamento> Departamento { get; set; }
+        public virtual DbSet<Empregado> Empregado { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=joao\sqlexpress;Database=db_teste1;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departamento>(entity =>
+            {
+                entity.Property(e => e.DepartamentoId).HasColumnName("DepartamentoID");
+
+                entity.Property(e => e.Nome).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Empregado>(entity =>
+            {
+                entity.Property(e => e.EmpregadoId).HasColumnName("EmpregadoID");
+
+                entity.Property(e => e.DepartamentoId).HasColumnName("DepartamentoID");
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+                entity.Property(e => e.Nome).HasMaxLength(50);
+
+                entity.Property(e => e.Sobrenome).HasMaxLength(50);
+
+                entity.HasOne(d => d.Departamento)
+                    .WithMany(p => p.Empregado)
+                    .HasForeignKey(d => d.DepartamentoId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Empregado_Departamento");
+            });
+        }
+    }
+}
