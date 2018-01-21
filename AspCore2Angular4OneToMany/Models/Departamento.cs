16ebbae6db72891671bb14e258a2d2ad224747df
joao <joao.vitor96silva@gmail.com>
2018-01-21 13:25:24 -0200

    Adicionar arquivos de projeto.

---
+++ AspCore2Angular4OneToMany/Models/Departamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCore2Angular4OneToMany.Models
+{
+    public partial class Departamento
+    {
+        public Departamento()
+        {
+            Empregado = new HashSet<Empregado>();
+        }
+
+        public int DepartamentoId { get; set; }
+        public string Nome { get; set; }
+
+        public ICollection<Empregado> Empregado { get; set; }
+    }
+}
